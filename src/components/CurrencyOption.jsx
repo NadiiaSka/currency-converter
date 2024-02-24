1a47f14b7111362ebcfb8270056655e48f124160
NadiiaSka <syvakivska@gmail.com>
2024-02-24 18:14:53 +0700

    add currency options autocomplete

---
+++ src/components/CurrencyOption.jsx
@@ -0,0 +1,40 @@
+import { Grid } from "@mui/material";
+
+const CurrencyOption = ({ countryName, currencyName, flagUrl }) => (
+  <Grid
+    container
+    alignItems="center"
+    sx={{
+      whiteSpace: "nowrap",
+      marginLeft: 0.1,
+    }}
+  >
+    <Grid
+      item
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        textOverflow: "clip",
+        marginBottom: 1,
+        marginLeft: 1,
+      }}
+    >
+      <img
+        src={flagUrl}
+        alt={countryName}
+        style={{
+          width: 24,
+          height: 24,
+          borderRadius: "50%",
+          marginRight: 7,
+        }}
+      />
+      <span style={{ marginLeft: 15 }}>
+        {currencyName} - {countryName}
+      </span>
+    </Grid>
+  </Grid>
+);
+
+export default CurrencyOption;
--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -1,33 +1,68 @@
 import { Autocomplete, Grid, TextField } from "@mui/material";
 import useAxios from "../hooks/useAxios";
+import CurrencyOption from "./CurrencyOption";
 
 const SelectCountry = () => {
   const [data] = useAxios("https://restcountries.com/v3.1/all");
 
   const dataFilter = data.filter((country) => "currencies" in country);
-  console.log(dataFilter);
+  const dataCountries = dataFilter.map((country) => {
+    return `${country.flag} ${Object.keys(country.currencies)[0]} - ${
+      country.name.common
+    }`;
+  });
+
+  const currencyOptions = dataFilter.map((country, index) => {
+    return {
+      id: index,
+      countryName: country.name.common,
+      currencyName: Object.keys(country.currencies)[0],
+      flagUrl: country.flags.png,
+    };
+  });
+
+  console.log(currencyOptions);
 
   return (
     <Grid item xs={12} md={3}>
       <Autocomplete
-        options={["USA", "Vietnam", "New Zealand"]}
-        id="country"
-        disableClearable
+        options={currencyOptions}
+        getOptionLabel={(option) => option.countryName}
+        renderOption={(props, option) => (
+          <CurrencyOption key={option.id} {...option} />
+        )}
         renderInput={(params) => (
           <TextField
             {...params}
-            label="to"
-            InputProps={{
-              sx: {
-                "& .MuiOutlinedInput-notchedOutline": {
-                  borderWidth: "1px",
-                  borderColor: "#333",
-                },
+            label="Select a country"
+            sx={{
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderWidth: "1px",
+                borderColor: "#333",
               },
             }}
           />
         )}
       />
+      {/* <Autocomplete
+        options={currencyOptions}
+        disableClearable
+        getOptionLabel={(option) => option.countryName}
+        renderOption={(option) => currencyOption(option)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            sx={{
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderWidth: "1px",
+                borderColor: "#333",
+              },
+            }}
+            placeholder="Select a country"
+          />
+        )}
+      /> */}
     </Grid>
   );
 };
