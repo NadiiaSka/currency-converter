9ac4e0918413ce9d56ebebe3b35565b3610e0985
NadiiaSka <syvakivska@gmail.com>
2024-02-25 15:45:16 +0700

    fix warning for incorrect value for a flag

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -35,22 +35,26 @@ const SelectCountry = (props) => {
         }}
         options={dataFilter}
         getOptionLabel={(option) => option.name.common}
-        renderOption={(props, option) => (
-          <Box
-            component="li"
-            sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
-            {...props}
-          >
-            <img
-              loading="lazy"
-              width="20"
-              srcSet={`https://flagcdn.com/w40/${option.altSpellings[0].toLowerCase()}.png 2x`}
-              src={`https://flagcdn.com/w20/${option.altSpellings[0].toLowerCase()}.png`}
-              alt=""
-            />
-            {Object.keys(option.currencies)[0]}- {option.name.common}
-          </Box>
-        )}
+        renderOption={(props, option) => {
+          if (option.altSpellings[0].length <= 2) {
+            return (
+              <Box
+                component="li"
+                sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+                {...props}
+              >
+                <img
+                  loading="lazy"
+                  width="20"
+                  srcSet={`https://flagcdn.com/w40/${option.altSpellings[0].toLowerCase()}.png 2x`}
+                  src={`https://flagcdn.com/w20/${option.altSpellings[0].toLowerCase()}.png`}
+                  alt=""
+                />
+                {Object.keys(option.currencies)[0]} - {option.name.common}
+              </Box>
+            );
+          }
+        }}
         renderInput={(params) => (
           <TextField
             {...params}
