25c1e91cbca73119af766fad9b4a307682678f5f
NadiiaSka <syvakivska@gmail.com>
2024-02-27 13:13:06 +0700

    switch currency

--- src/components/SwitchCurrency.jsx
+++ src/components/SwitchCurrency.jsx
@@ -1,10 +1,20 @@
 import { Button, Grid } from "@mui/material";
 import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
+import { CurrencyContext } from "../context/CurrencyContext";
+import { useContext } from "react";
 
 const SwitchCurrency = () => {
+  const { fromCurrency, setFromCurrency, toCurrency, setToCurrency } =
+    useContext(CurrencyContext);
+
+  const handleSwitch = () => {
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  };
+
   return (
     <Grid item xs={12} md="auto">
-      <Button sx={{ borderRadius: 1, height: "100%" }}>
+      <Button onClick={handleSwitch} sx={{ borderRadius: 1, height: "100%" }}>
         <CompareArrowsIcon sx={{ fontSize: 30 }} />
       </Button>
     </Grid>
